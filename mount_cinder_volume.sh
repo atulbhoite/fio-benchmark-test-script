7f8c2e603b632d6f3f60e38da620eead9df0fdba
Atul S. Bhoite <abhoite@godaddy.com>
2017-06-21 11:24:08 -0700

    Added instructions to script to create a dummy test file on new cinder mount.

--- mount_cinder_volume.sh
+++ mount_cinder_volume.sh
@@ -1,9 +1,15 @@
 #! /bin/bash
 
+fdisk -l
+
 echo "Creating file system on /dev/vdb..."
 mkfs.ext4 /dev/vdb
 
 echo "Mounting cinder volume..."
 mkdir -p /mnt/testCinder01/
 mount /dev/vdb /mnt/testCinder01/
+
+echo " Creating dummy file on newly mounted volume..."
+echo "This is a dummy file on the new cinder volume. " > /mnt/testCinder01/mytestfile
+cat /mnt/testCinder01/mytestfile
 
