c164d5b6ba9d6a18346a8c9c03b5c5926e0a30bb
Atul S. Bhoite <abhoite@godaddy.com>
2017-07-27 10:44:17 -0700

    ensure 30 second default run for all tests

--- loadfio.sh
+++ loadfio.sh
@@ -4,7 +4,7 @@ echo "Running loadfio.sh... "
 
 if [ -z "${FIO_TEST_RUNTIME}" ]; then
     echo "runtime for test is not set using the variable FIO_TEST_RUNTIME, running default time of 30 seconds";
-    ./generate_fio_scripts.sh 3;
+    ./generate_fio_scripts.sh 30;
 else
     echo "using runtime specified in variable FIO_TEST_RUNTIME ${FIO_TEST_RUNTIME}";
     ./generate_fio_scripts.sh "${FIO_TEST_RUNTIME}";
