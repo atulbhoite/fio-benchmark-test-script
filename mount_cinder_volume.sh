87b8ff76bd527c216615c9fc6c7946acdbe7900f
Atul S. Bhoite <abhoite@godaddy.com>
2017-07-31 15:28:41 -0700

    Added command to display status of mount and redirect it to the commands_result.txt file.

--- mount_cinder_volume.sh
+++ mount_cinder_volume.sh
@@ -8,6 +8,7 @@ mkfs.ext4 /dev/vdb
 echo "Mounting cinder volume..."
 mkdir -p /mnt/testCinder01/
 mount /dev/vdb /mnt/testCinder01/
+df -kH
 
 echo " Creating dummy file on newly mounted volume..."
 echo "This is a dummy file on the new cinder volume. " > /mnt/testCinder01/mytestfile
