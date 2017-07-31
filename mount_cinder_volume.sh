bf7607284e1ea2201034f97bb4f694204ba31f34
Atul S. Bhoite <abhoite@godaddy.com>
2017-07-31 13:33:04 -0700

    Attempting to run commands as sudo

--- mount_cinder_volume.sh
+++ mount_cinder_volume.sh
@@ -3,13 +3,13 @@
 fdisk -l
 
 echo "Creating file system on /dev/vdb..."
-mkfs.ext4 /dev/vdb
+sudo mkfs.ext4 /dev/vdb
 
 echo "Mounting cinder volume..."
-mkdir -p /mnt/testCinder01/
-mount /dev/vdb /mnt/testCinder01/
+sudo mkdir -p /mnt/testCinder01/
+sudo mount /dev/vdb /mnt/testCinder01/
 
 echo " Creating dummy file on newly mounted volume..."
 echo "This is a dummy file on the new cinder volume. " > /mnt/testCinder01/mytestfile
-cat /mnt/testCinder01/mytestfile
+sudo cat /mnt/testCinder01/mytestfile
 
